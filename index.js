7e59b77d982768204e50921fb37d50430e7a66de
abhirajn <abhin.tmk@gmail.com>
2023-10-04 17:44:22 +0530

    updated port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const Analysis = require('./Routes/Analysis')
 const cors = require('cors');
-
+require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +13,6 @@ app.use('/api' , Analysis)
 app.get('/' , (req , res)=>{
     res.send("hello world")
 })
-app.listen(3000 , (req,res)=>[
-    console.log("server started at port 3000")
+app.listen(process.env.PORT || 8000 , (req,res)=>[
+    console.log("server started at port 8000")
 ])
